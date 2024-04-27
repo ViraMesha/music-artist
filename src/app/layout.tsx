edb944587e2c43210e402d838018d968f6144e6f
ViraMesha <wira.mescha@gmail.com>
2024-04-27 21:27:37 +0300

    feat: Add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Alex_Brush, Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const alexBrush = Alex_Brush({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-alexBrush",
+});
+
+const montserrat = Montserrat({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${alexBrush.variable} ${montserrat.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
