a069f4334565c9a79edbdd5de03d5173e1adce6c
ViraMesha <wira.mescha@gmail.com>
2024-05-10 23:14:36 +0300

    feat: Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,68 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { Nav, Socials } from "./index";
+import Image from "next/image";
+import { logoImg } from "@/utils";
+import { fadeIn } from "@/helpers/variants";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-accent section h-[500px] xl:h-auto">
+      <div className="container mx-auto h-full flex flex-col items-center gap-y-8">
+        {/* email link */}
+        <motion.div
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+        >
+          <Link href="#">
+            <h2 className="text-[24px] lg:text-[38px] font-semibold leading-tight">
+              hello@miareynolds.com
+            </h2>
+          </Link>
+        </motion.div>
+        {/* nav */}
+        <motion.div
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+        >
+          <Nav
+            containerStyles="flex-center flex-col xl:flex-row gap-y-4 xl:gap-x-8 text-sm uppercase font-semibold"
+            linkStyles="hover:text-primary/80 transition-all"
+          />
+        </motion.div>
+        {/* socials */}
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+        >
+          <Socials
+            containerStyles="flex text-[24px] gap-x-8"
+            iconStyles="hover:text-primary/80 transition-all justify-center"
+          />
+        </motion.div>
+        {/* logo */}
+        <motion.div
+          variants={fadeIn("up", 0.8)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.1 }}
+        >
+          <Link
+            href="/"
+            className="relative w-[250px] h-[50px] flex transition-all"
+          >
+            <Image src={logoImg} alt="logo" fill className="object-contain" />
+          </Link>
+        </motion.div>
+      </div>
+    </footer>
+  );
 };
